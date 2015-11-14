7ed79401aac4e2827786ae09110b061a29625f69
Tanner <eeriecrate@gmail.com>
2015-11-14 16:44:09 -0500

    Added pausing and growth to test game

--- TheTest/main.js
+++ TheTest/main.js
@@ -7,9 +7,9 @@ canvas.style.height = canvas.height + "px";
 var ctx = canvas.getContext('2d');
 
 Game = {};
-Game.paused = false;
+Game.paused = true;
 Game.fps = 60;
-Game.Gravity = 6;
+Game.Gravity = 10;
 
 Art = {};
 
@@ -19,6 +19,8 @@ KeysDown = {
 	s: false,
 	y: false
 };
+
+var EscapeHit = false;
 
 $(document).keydown(function(key){
 	if(key.keyCode == 32){
@@ -30,8 +32,25 @@ $(document).keydown(function(key){
 	if(key.keyCode == 68){
 		KeysDown.d = true;
 	};
-	if(key.keyCode = 89){
+	if(key.keyCode == 89){
 		KeysDown.y = true;
+	};
+	if(key.keyCode == 27 && !EscapeHit){
+		EscapeHit = true;
+		if(Game.paused){
+			Game.paused = false;
+		}
+		else{
+			Game.paused = true;
+			ctx.rect((canvas.width/2)-150,(canvas.height/2)-50,300,100);
+			ctx.fillStyle = "Black";
+			ctx.fill();
+			ctx.stroke();
+			ctx.font = "20px Georgia";
+			ctx.fillStyle = "White";
+			ctx.fillText("GAME PAUSED",(canvas.width/2)-75,(canvas.height/2)+12.5);
+		}
+
 	};
 });
 $(document).keyup(function(key){
@@ -44,9 +63,12 @@ $(document).keyup(function(key){
 	if(key.keyCode == 68){
 		KeysDown.d = false;
 	};
-	if(key.keyCode = 89){
+	if(key.keyCode == 89){
 		KeysDown.y = false;
 	};
+	if(key.keyCode == 27){
+		EscapeHit = false;
+	}
 });
 
 Art.Circle = function(x,y,r){
@@ -64,10 +86,11 @@ Art.DrawCircle = function(c){
 
 var Player = new Art.Circle(100,100,40);
 Player.Jumping = false;
-Player.Standing = null;
-Player.MaxJump = 30;
-Player.JumpV = 5;
+Player.Standing = false;
+Player.MaxJump = 25;
 Player.Jump = 0;
+Player.Morphing = false;
+Player.Change = 0;
 Player.requestJump = function(){
 	if (Player.Standing){
 		Player.Standing = false;
@@ -75,21 +98,28 @@ Player.requestJump = function(){
 		Player.Jumping = true;
 	};
 };
-
+Player.requestMorph = function(){
+	if(!Player.Morphing){
+		Player.Morphing = true;
+		if(Player.r == 40){
+			Player.Change = 5;
+		}else{
+			Player.Change = -5;
+		}
+	}
+}
 Art.DrawCircle(Player)
 
 Game.main = function(){
+	if(!Game.paused){
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
 	if(Player.Jumping){
-		Player.y += -Player.JumpV;
+		Player.y += -Player.Jump;
 		Player.Jump = Player.Jump - 1;
 		if(Player.Jump == 0){
 			Player.Jumping = false;
 		}
 	}else{
-		for(var i = 0; i <= Game.Gravity; i += 1){
-
-		};
 		if((Player.y+Player.r)>=(canvas.height)){
 			Player.yv = 0;
 			Player.y = canvas.height-Player.r
@@ -100,18 +130,34 @@ Game.main = function(){
 			Player.Standing = false;
 		}
 	}
+	if(Player.Morphing){
+		if(Player.Change == 5){
+			Player.r += Player.Change
+			if(Player.r == 120){
+				Player.Morphing = false;
+			}
+		}else{
+			Player.r += Player.Change
+			if(Player.r == 40){
+				Player.Morphing = false;
+			}
+		}
+	}
 	if(KeysDown.a){
-		Player.x += -3;
+		Player.x += -6;
 	}
 	if(KeysDown.d){
-		Player.x += 3;
+		Player.x += 6;
 	}
-
+	if(KeysDown.y){
+		Player.requestMorph();
+	}
 	if(KeysDown.s){
 		Player.requestJump();
 	}
 	Player.y += Player.yv;
 	Art.DrawCircle(Player);
+}
 };
 
 Game._intervalId = setInterval(Game.main, (1000/Game.fps))
