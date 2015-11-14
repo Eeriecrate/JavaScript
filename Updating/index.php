1ad66fcd136682024b293aa7a366a4a7d386ffe6
Tanner <eeriecrate@gmail.com>
2015-11-14 10:08:15 -0500

    Added a better counter using innerHTML

--- Updating/index.php
+++ Updating/index.php
@@ -3,19 +3,25 @@
 		<title>Update Test</title>
 	</head>
 	<body>
+		<h3>Counter: <span style = "text-decoration: underline;" class = "counter"></span> </h3>
 		<script>
+			var Counter = document.getElementsByClassName("counter")[0];
 			var Game = {
 				FPS: 60,
 				Paused: false,
-				Count: 0
+				Count: 0,
 			};
 			Game.update = function() {
 				if (!Game.Paused) {
 				Game.Count += 1;
-				document.write(Game.Count);
+				updateCounter(Game.Count);
 				};
 			};
 			
+			function updateCounter(Count){
+				Counter.innerHTML = Count;
+			};
+
 			//This creates the loop
 			Game._intervalId = setInterval(Game.update, (1000/Game.FPS))
 
